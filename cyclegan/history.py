9d2c49034932dd52644b3cd6e57b64db70282315
Alex Nichol <unixpickle@gmail.com>
2018-01-04 15:21:50 -0800

    image histories

---
+++ cyclegan/history.py
@@ -0,0 +1,44 @@
+"""
+An in-graph image history buffer.
+"""
+
+import tensorflow as tf
+
+def history_image(image, buffer_size=50, name='image_buffer'):
+    """
+    Get an image from a history buffer and submit the
+    image to the same buffer.
+
+    Args:
+      image: an image Tensor. The shape must be known in
+        advance.
+      buffer_size: the number of images to store in the
+        history buffer.
+      name: the default name of the scope in which the
+        buffer is stored. Uniquified as needed.
+    """
+    with tf.variable_scope(None, default_name=name):
+        buf = tf.get_variable('images', shape=[buffer_size] + [x.value for x in image.get_shape()],
+                              dtype=image.dtype, trainable=False)
+        size = tf.get_variable('size', dtype=tf.int32, initializer=0)
+        def _insert_new():
+            insert_idx = tf.assign_add(size, 1) - 1
+            return _assign_buf_entry(buf, insert_idx, image)
+        def _sample_old():
+            idx = tf.random_uniform((), maxval=buffer_size, dtype=tf.int32)
+            # `+ 0` hack to deal with buffer_size == 1.
+            # See https://github.com/tensorflow/tensorflow/issues/4663.
+            old = buf[idx] + 0
+            with tf.control_dependencies([old]):
+                assign = _assign_buf_entry(buf, idx, image)
+                with tf.control_dependencies([assign]):
+                    return tf.identity(old)
+        return tf.cond(size < buffer_size,
+                       _insert_new,
+                       lambda: tf.cond(tf.random_uniform(()) < 0.5,
+                                       _sample_old,
+                                       lambda: image))
+
+def _assign_buf_entry(buf, idx, image):
+    pieces = [buf[:idx], tf.expand_dims(image, 0), buf[idx+1:]]
+    return tf.assign(buf, tf.concat(pieces, 0))[idx]
---
+++ cyclegan/test_history.py
@@ -0,0 +1,58 @@
+"""Tests for image histories."""
+
+# pylint: disable=E1129
+
+import tensorflow as tf
+
+from .history import history_image
+
+def test_buffered_image_append():
+    """Test underfull image histories"""
+    with tf.Graph().as_default():
+        in_image = tf.random_normal((5, 5))
+        hist = history_image(in_image, buffer_size=5)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for _ in range(5):
+                in_arr, out_arr = sess.run((in_image, hist))
+                assert (in_arr == out_arr).all()
+
+def test_buffered_image_sample():
+    """Test sampling from image histories"""
+    with tf.Graph().as_default():
+        in_image = tf.random_normal((5, 5))
+        hist = history_image(in_image, buffer_size=5)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            history = []
+            for _ in range(5):
+                history.append(sess.run(hist))
+            sample_count = 0
+            while sample_count < 5:
+                in_arr, out_arr = sess.run((in_image, hist))
+                if (in_arr == out_arr).all():
+                    continue
+                found = False
+                for i, hist_entry in enumerate(history):
+                    if (hist_entry == out_arr).all():
+                        found = True
+                        history[i] = in_arr
+                assert found
+                sample_count += 1
+
+def test_buffered_image_single():
+    """Test a buffer with one sample"""
+    with tf.Graph().as_default():
+        in_image = tf.random_normal((5, 5))
+        hist = history_image(in_image, buffer_size=1)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            history = sess.run(hist)
+            sample_count = 0
+            while sample_count < 5:
+                in_arr, out_arr = sess.run((in_image, hist))
+                if (in_arr == out_arr).all():
+                    continue
+                assert (history == out_arr).all()
+                history = in_arr
+                sample_count += 1
