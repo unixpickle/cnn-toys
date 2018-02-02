c634f13f15d01976918c94e16166441b484e92d4
Alex Nichol <unixpickle@gmail.com>
2018-02-02 10:31:31 -0800

    more flexible CycleGAN model
    This way, you can use cnn_toys.cyclegan as an API and change the model architecture easily.

--- cnn_toys/cyclegan/model.py
+++ cnn_toys/cyclegan/model.py
@@ -1,5 +1,5 @@
 """
-Architectures for GANs.
+Architectures for CycleGANs.
 """
 
 import tensorflow as tf
@@ -9,16 +9,22 @@ from .history import history_image
 # pylint: disable=R0902,R0903
 class CycleGAN:
     """
-    A class representing all the components of a CycleGAN
-    in a single package.
+    A CycleGAN model.
+
+    The generator() and discriminator() methods can be
+    overridden to change the model architecture.
+
+    Unless otherwise stated, all pixels are assumed to
+    range from [0, 1].
     """
     def __init__(self, real_x, real_y, buffer_size=50, cycle_weight=10):
         initializer = tf.truncated_normal_initializer(stddev=0.02)
+        self.buffer_size = buffer_size
         with tf.variable_scope('cyclegan', initializer=initializer):
             self.real_x = _add_image_noise(real_x)
             self.real_y = _add_image_noise(real_y)
             self._setup_generators()
-            self._setup_discriminators(buffer_size)
+            self._setup_discriminators()
             self._setup_cycles(cycle_weight)
             self._setup_gradients()
 
@@ -27,29 +33,37 @@ class CycleGAN:
         opt = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
         return opt.apply_gradients(self.gradients, global_step=global_step)
 
+    def generator(self, image):
+        """Apply the generator to an image."""
+        return standard_generator(image, self._num_residual())
+
+    def discriminator(self, images): # pylint: disable=R0201
+        """Apply the descriminator to a batch of images."""
+        return standard_discriminator(images)
+
     def _setup_generators(self):
         start_vars = tf.trainable_variables()
         with tf.variable_scope('gen_x'):
-            self.gen_x = generator(self.real_y, self._num_residual())
+            self.gen_x = self.generator(self.real_y)
         with tf.variable_scope('gen_y'):
-            self.gen_y = generator(self.real_x, self._num_residual())
+            self.gen_y = self.generator(self.real_x)
         self.gen_vars = [v for v in tf.trainable_variables() if v not in start_vars]
 
-    def _setup_discriminators(self, buffer_size):
+    def _setup_discriminators(self):
         start_vars = tf.trainable_variables()
         with tf.variable_scope('disc_x'):
-            disc_x_loss, gen_x_loss = gan_loss(self.real_x, self.gen_x, buffer_size)
+            disc_x_loss, gen_x_loss = self._discriminate(self.real_x, self.gen_x)
         with tf.variable_scope('disc_y'):
-            disc_y_loss, gen_y_loss = gan_loss(self.real_y, self.gen_y, buffer_size)
+            disc_y_loss, gen_y_loss = self._discriminate(self.real_y, self.gen_y)
         self.disc_vars = [v for v in tf.trainable_variables() if v not in start_vars]
         self.disc_loss = (disc_x_loss + disc_y_loss) / 2
         self.gen_loss = gen_x_loss + gen_y_loss
 
     def _setup_cycles(self, weight):
         with tf.variable_scope('gen_y', reuse=True):
-            self.cycle_y = generator(self.gen_x, self._num_residual())
+            self.cycle_y = self.generator(self.gen_x)
         with tf.variable_scope('gen_x', reuse=True):
-            self.cycle_x = generator(self.gen_y, self._num_residual())
+            self.cycle_x = self.generator(self.gen_y)
         self.cycle_loss = weight * (tf.reduce_mean(tf.abs(self.real_x - self.cycle_x)) +
                                     tf.reduce_mean(tf.abs(self.real_y - self.cycle_y)))
 
@@ -59,13 +73,35 @@ class CycleGAN:
         total_grad = _add_grad_dicts(gen_grad, disc_grad)
         self.gradients = [(g, v) for v, g in total_grad.items()]
 
+    def _discriminate(self, real_image, gen_image):
+        """
+        Run samples through a discriminator to get the GAN
+        losses.
+
+        Returns:
+          A tuple (discriminator_loss, generator_loss).
+        """
+        buf_image = history_image(gen_image, self.buffer_size)
+        batch = tf.stack([real_image, gen_image, buf_image])
+        disc = self.discriminator(batch)
+        real_outs, gen_outs, buf_outs = disc[0], disc[1], disc[2]
+        disc_loss = tf.reduce_mean(tf.square(real_outs - 1)) + tf.reduce_mean(tf.square(buf_outs))
+        gen_loss = tf.reduce_mean(tf.square(gen_outs - 1))
+        return disc_loss, gen_loss
+
     def _num_residual(self):
         if self.real_x.get_shape()[1].value > 128:
             return 9
         return 6
 
-def discriminator(images):
-    """Get a batch of discriminator outputs, one per image."""
+def standard_discriminator(images):
+    """
+    Apply the standard CycleGAN discriminator to a batch
+    of images.
+
+    The output Tensor may have any rank, but the outer
+    dimension must match the batch size.
+    """
     activation = tf.nn.leaky_relu
     outputs = 2 * images - 1
     outputs = tf.layers.conv2d(outputs, 64, 4, strides=2, activation=activation)
@@ -77,18 +113,16 @@ def discriminator(images):
         outputs = activation(instance_norm(outputs))
     return tf.layers.conv2d(outputs, 1, 1)
 
-def gan_loss(real_image, gen_image, buffer_size):
-    """Apply a discriminator and get (disc_loss, gen_loss)."""
-    buf_image = history_image(gen_image, buffer_size)
-    batch = tf.stack([real_image, gen_image, buf_image])
-    disc = discriminator(batch)
-    real_outs, gen_outs, buf_outs = disc[0], disc[1], disc[2]
-    disc_loss = tf.reduce_mean(tf.square(real_outs - 1)) + tf.reduce_mean(tf.square(buf_outs))
-    gen_loss = tf.reduce_mean(tf.square(gen_outs - 1))
-    return disc_loss, gen_loss
+def standard_generator(image, num_residual):
+    """
+    Apply the standard CycleGAN generator to an image.
 
-def generator(image, num_residual):
-    """Generate an image in Y using an image in X."""
+    Args:
+      image: an image Tensor with pixel values in the
+        range [0, 1].
+      num_residual: the number of residual layers. In the
+        original paper, this varied by image size.
+    """
     activation = lambda x: tf.nn.relu(instance_norm(x))
     output = 2 * image - 1
     output = reflection_pad(tf.expand_dims(output, 0), 7)
