4b07f46388f2bb448b6e7ca295aab1801c4ab0b7
Alex Nichol <unixpickle@gmail.com>
2018-02-02 10:37:20 -0800

    add some imports

--- cnn_toys/cyclegan/__init__.py
+++ cnn_toys/cyclegan/__init__.py
@@ -1,1 +1,5 @@
 """The CycleGAN architecture."""
+
+from .history import history_image
+from .model import (CycleGAN, standard_discriminator, standard_generator, instance_norm,
+                    reflection_pad)
