207a118da865daf0b109feed93f76f9049a595de
Alex Nichol <unixpickle@gmail.com>
2018-04-29 17:54:52 -0700

    real_nvp: script to sample

--- cnn_toys/real_nvp/run_sample.py
+++ cnn_toys/real_nvp/run_sample.py
@@ -1,20 +1,16 @@
-"""Train a real NVP model."""
+"""Sample a real NVP model."""
 
 import argparse
 from functools import partial
-from itertools import count
 
 import tensorflow as tf
 
 import cnn_toys.real_nvp as nvp
-from cnn_toys.data import dir_dataset
-from cnn_toys.saving import save_state, restore_state
+from cnn_toys.graphics import save_image_grid
+from cnn_toys.saving import restore_state
 
 def main(args):
-    """The main training loop."""
-    print('loading dataset...')
-    dataset = dir_dataset(args.data_dir, args.size)
-    images = dataset.repeat().batch(args.batch).make_one_shot_iterator().get_next()
+    """Sampling entry-point."""
     print('setting up model...')
     main_layers = [
         nvp.MaskedConv(partial(nvp.checkerboard_mask, True), 3),
@@ -28,28 +24,27 @@ def main(args):
     ]
     network = nvp.Network([nvp.PaddedLogit()] + (main_layers * 3))
     with tf.variable_scope('model'):
-        loss = -tf.reduce_mean(nvp.log_likelihood(network, images))
-    optimize = tf.train.AdamOptimizer(learning_rate=args.step_size).minimize(loss)
+        fake_batch = tf.zeros((args.rows * args.cols, args.size, args.size, 3), dtype=tf.float32)
+        _, latents, _ = network.forward(fake_batch)
+    with tf.variable_scope('model', reuse=True):
+        gauss_latents = [tf.random_normal(latent.shape) for latent in latents]
+        images = network.inverse(None, gauss_latents)
     with tf.Session() as sess:
         print('initializing variables...')
         sess.run(tf.global_variables_initializer())
         print('attempting to restore model...')
         restore_state(sess, args.state_file)
-        print('training...')
-        for i in count():
-            cur_loss, _ = sess.run((loss, optimize))
-            print('step %d: loss=%f' % (i, cur_loss))
-            if i % args.save_interval == 0:
-                save_state(sess, args.state_file)
+        print('generating samples...')
+        samples = sess.run(tf.reshape(images, [args.rows, args.cols, args.size, args.size, 3]))
+        save_image_grid(samples, args.out_file)
 
 def _parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--data-dir', help='data directory', default='data')
     parser.add_argument('--size', help='image size', type=int, default=64)
-    parser.add_argument('--batch', help='batch size', type=int, default=32)
-    parser.add_argument('--step-size', help='training step size', type=float, default=2e-4)
+    parser.add_argument('--rows', help='rows in output', type=int, default=4)
+    parser.add_argument('--cols', help='columns in output', type=int, default=4)
     parser.add_argument('--state-file', help='state output file', default='state.pkl')
-    parser.add_argument('--save-interval', help='iterations per save', type=int, default=100)
+    parser.add_argument('--out-file', help='image output file', default='samples.png')
     return parser.parse_args()
 
 if __name__ == '__main__':
