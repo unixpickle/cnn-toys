real_nvp: fix objective sign error

cnn_toys/real_nvp/objective.py
@@ -15,7 +15,7 @@ def bits_per_pixel(layer, inputs, noise=1.0 / 255.0):
     sampled_noise = tf.random_uniform(tf.shape(inputs), maxval=noise)
     log_probs = log_likelihood(layer, inputs + sampled_noise)
     num_pixels = int(np.prod([x.value for x in inputs.get_shape()[1:]]))
-    return (log_probs / num_pixels - tf.log(float(noise))) / tf.log(2.0)
+    return -(log_probs / num_pixels - tf.log(float(noise))) / tf.log(2.0)
 
 def log_likelihood(layer, inputs):
     """